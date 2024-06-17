f294e7dadea89e6e0283e56c0b608ea837d5e208
LeahyJK <leahyjared2@gmail.com>
2024-06-17 10:29:54 -0400

    done

--- input.js
+++ input.js
@@ -3,11 +3,13 @@ let messages = document.querySelector(".messages");
 
 // CODE ALONG, PART 1 CONTINUED
 // 3. Declare a variable to store the input element
-
+let userInput = document.querySelector(".userinput")
 
 submitButton.addEventListener("click", function() {
     // 4. Declare a variable to store the value of the input
+    let inputVal = userInput.value;
     // 5. Use the console to check a value is saved.
+    console.log(inputVal);
     //  - Type something and click the button to confirm it works!
 
 
@@ -15,6 +17,7 @@ submitButton.addEventListener("click", function() {
 
     // CODE ALONG, PART 2
     // 6. User .innerHTML to display the value to the messages div.
+    messages.innerHTML=(inputVal);
     // 7. Type something and test it out.
 
 
